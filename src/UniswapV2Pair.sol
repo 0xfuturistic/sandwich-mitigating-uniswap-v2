3ede83b99bf0556663b82be1636842785a191d41
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-07-22 21:02:04 -0400

    fix typos

--- src/UniswapV2Pair.sol
+++ src/UniswapV2Pair.sol
@@ -214,7 +214,7 @@ contract UniswapV2Pair is UniswapV2ERC20 {
             SwapType swapType = amount0Out > 0 ? SwapType.BUY : SwapType.SELL;
 
             if (sequencingRuleInfo.emptyBuysOrSells) {
-                // If we have run out of buys or sells, the swap type must be the same as the last one
+                // If we have ran out of buys or sells, the swap type must be the same as for the last swap 
                 require(swapType == sequencingRuleInfo.lastSwapType, 'UniswapV2: Swap violates sequencing rule');
             } else {
                 // Find the required swap type so we can validate against it
