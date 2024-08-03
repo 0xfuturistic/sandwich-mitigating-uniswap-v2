a86305d1f7888f4332bde7e9997a534ce7d8f259
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-08-02 18:14:56 -0700

    refactor tests

--- test/SandwichResistanceTest.t.sol
+++ test/SandwichResistanceTest.t.sol
@@ -29,85 +29,41 @@ contract SandwichResistanceTest is Test {
         pair.mint(address(this));
     }
 
-    function test_simpleSandwich_fails(uint256) public {
+    function test_simpleSandwich_fails() public {
         // first buy
-        token1.transfer(address(pair), getPrice() * 1 ether);
-        // the denominator is 0.95 because of fees
-        pair.swap(0.95 ether / getPrice(), 0, address(this), "");
+        token1.transfer(address(pair), 1 ether);
+        pair.swap(0.99 ether, 0, address(this), "");
 
         // second buy
-        pair.swap(0.95 ether / getPrice(), 0, address(this), "");
+        token0.transfer(address(pair), 1 ether);
+        pair.swap(0.83 ether, 0, address(this), "");
 
         // sell
+        token0.transfer(address(pair), 1 ether);
+        vm.expectRevert("UniswapV2: Swap violates sequencing rule");
+        pair.swap(0, 0.99 ether, address(this), "");
     }
 
     function test_complexSandwich_fails() public {
         // first buy
-        token0.transfer(address(pair), 1 ether);
-        token1.transfer(address(pair), 2 ether);
-        pair.mint(address(this));
-
-        token0.transfer(address(pair), 0.1 ether);
-        pair.swap(0.09 ether, 0, address(this), "");
+        token1.transfer(address(pair), 1 ether);
+        pair.swap(0.99 ether, 0, address(this), "");
 
         // sell
-        token0.transfer(address(pair), 0.1 ether);
-        pair.swap(0, 0.09 ether, address(this), "");
+        token0.transfer(address(pair), 1 ether);
+        pair.swap(0, 0.99 ether, address(this), "");
 
         // second buy
         token0.transfer(address(pair), 1 ether);
-        token1.transfer(address(pair), 2 ether);
-        pair.mint(address(this));
-
-        token0.transfer(address(pair), 0.1 ether);
-        pair.swap(0.09 ether, 0, address(this), "");
+        pair.swap(0.83 ether, 0, address(this), "");
 
         // third buy
         token0.transfer(address(pair), 1 ether);
-        token1.transfer(address(pair), 2 ether);
-        pair.mint(address(this));
-
-        token0.transfer(address(pair), 0.1 ether);
-        pair.swap(0.09 ether, 0, address(this), "");
+        pair.swap(0.59 ether, 0, address(this), "");
 
         // sell
-        token0.transfer(address(pair), 0.1 ether);
+        token0.transfer(address(pair), 1 ether);
         vm.expectRevert("UniswapV2: Swap violates sequencing rule");
-        pair.swap(0, 0.09 ether, address(this), "");
-    }
-
-    function test_emptySells_succeeds() public {
-        // first buy
-        token0.transfer(address(pair), 1 ether);
-        token1.transfer(address(pair), 2 ether);
-        pair.mint(address(this));
-
-        token0.transfer(address(pair), 0.1 ether);
-        pair.swap(0.09 ether, 0, address(this), "");
-
-        // sell
-        token0.transfer(address(pair), 0.1 ether);
-        pair.swap(0, 0.09 ether, address(this), "");
-
-        // second buy
-        token0.transfer(address(pair), 1 ether);
-        token1.transfer(address(pair), 2 ether);
-        pair.mint(address(this));
-
-        token0.transfer(address(pair), 0.1 ether);
-        pair.swap(0.09 ether, 0, address(this), "");
-
-        // third buy
-        token0.transfer(address(pair), 1 ether);
-        token1.transfer(address(pair), 2 ether);
-        pair.mint(address(this));
-
-        token0.transfer(address(pair), 0.1 ether);
-        pair.swap(0.09 ether, 0, address(this), "");
-    }
-
-    function getPrice() public view returns (uint256) {
-        (uint112 _reserve0, uint112 _reserve1,) = pair.getReserves();
-        return _reserve1 / _reserve0;
+        pair.swap(0, 0.99 ether, address(this), "");
     }
 }
